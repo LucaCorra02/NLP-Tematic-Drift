75b47f85bb9394adca77de677f729d16915236a0
LucaCorra02 <luca.corradini4@gmail.com>
2026-05-05 12:04:41 +0200

    refactor

--- Src/Novelity-Resonance/find_pioner.py
+++ Src/Novelity-Resonance/find_pioner.py
@@ -5,9 +5,10 @@ import numpy as np
 import pandas as pd
 
 class PioneerAnalyzer:
-    def __init__(self, similarity_path, data_path):
+    def __init__(self, similarity_path, data_path, metric_ris_path):
         self.similarity_path = Path(similarity_path)
         self.data_path = Path(data_path)
+        self.metric_ris_path = Path(metric_ris_path)
         tmp_df_sim = pd.read_parquet(self.similarity_path)
         tmp_df_data =  pd.read_parquet(self.data_path)
         self.df_merged = pd.merge(
@@ -27,11 +28,17 @@ class PioneerAnalyzer:
         - Transience = Distance between a specific paper and the papers published in n future years (1 - cosine_future)
         - Resonance [-1,1] = Novelty - Transience 
     """
-    def calculate_metrics(self, k_mean = 10 ,year_window = 2):
+    def calculate_metrics(self, k_mean = 10 ,year_window = 2,):
         years = self.df_merged["publication_year"].unique()
         valid_years_mask = [True if year - year_window in years and year + year_window in years else False for year in years ]
         valid_years = years[valid_years_mask]
         all_paper_years = self.df_merged["publication_year"].values
+
+        year_indices = {}
+        for y in years:
+            past_idx = np.where((all_paper_years >= y - year_window) & (all_paper_years < y))[0]
+            future_idx = np.where((all_paper_years > y) & (all_paper_years <= y + year_window))[0]
+            year_indices[y] = (past_idx, future_idx)
 
         results = {}
         for id_paper in range(len(self.df_merged)):
@@ -39,20 +46,20 @@ class PioneerAnalyzer:
             if year_paper not in valid_years:
                 results[id_paper] = {"Novelty": np.nan, "Transience": np.nan, "Resonance": np.nan}
                 continue
-            past_mask_index = np.where((all_paper_years < year_paper) & (all_paper_years >= year_paper - year_window))[0]
-            future_mask_index = np.where((all_paper_years > year_paper) & (all_paper_years <= year_paper + year_window))[0]
 
-            novelity = self._calculate_similarity_masked(id_paper, past_mask_index, k_mean)
+            past_mask_index, future_mask_index = year_indices[year_paper]
+            novelty = self._calculate_similarity_masked(id_paper, past_mask_index, k_mean)
             transience = self._calculate_similarity_masked(id_paper, future_mask_index, k_mean)
-            resonance = novelity - transience # Sim_Future - Sim_Past
-            results[id_paper] = {"Novelty": float(novelity), "Transience": float(transience), "Resonance": float(resonance)}
+            resonance = novelty - transience # Sim_Future - Sim_Past
+            results[id_paper] = {"Novelty": float(novelty), "Transience": float(transience), "Resonance": float(resonance)}
 
         df_results = pd.DataFrame.from_dict(results, orient="index")
-        print(len(df_results))
+        df_results.reset_index(inplace=True)
+        df_results.rename(columns={'index': 'id'}, inplace=True)
+        print("before: ", len(df_results))
         df_results = df_results.dropna()
-        print(len(df_results))
-        print(max(df_results["Resonance"]))
-
+        print("after: ",len(df_results))
+        df_results.to_csv(self.metric_ris_path, index=False)
 
     def _calculate_similarity_masked(self, id_paper: int, mask_index: np.array, k_mean):
         if len(mask_index) > 0:
@@ -65,6 +72,7 @@ class PioneerAnalyzer:
 if __name__ == "__main__":
     analysis = PioneerAnalyzer(
         similarity_path="../Embeddings/Similarity/similarity.parquet",
-        data_path="../Data/Raw/scraped_data_cleaned.parquet"
+        data_path="../Data/Raw/scraped_data_cleaned.parquet",
+        metric_ris_path = "metric.csv"
     )
-    analysis.calculate_metrics(k_mean=10, year_window=3)
+    analysis.calculate_metrics(k_mean=30, year_window=3)
